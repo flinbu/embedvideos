d85f7149ae357497fb1636453c5cff42dfe62c38
flinbu <me@flinbu.co>
2017-10-28 17:11:20 -0500

    sandbox

--- embed.videos.js
+++ embed.videos.js
@@ -117,10 +117,10 @@ window.kalturaLoaded = false;
               }
             });
           } else if (settings.video_src == 'youtube') {
-            var video_player = '<' + tag + ' width="1920" height="1080" layout="responsive" src="https://www.youtube.com/embed/' + settings.video_id + '?autoplay=' + videoAutoPlay + '" frameborder = "0" allowfullscreen autoplay >' + insideContent + '< /' + tag + '>';
+            var video_player = '<' + tag + ' width="1920" height="1080" sandbox="allow-scripts allow-same-origin" layout="responsive" src="https://www.youtube.com/embed/' + settings.video_id + '?autoplay=' + videoAutoPlay + '" frameborder = "0" allowfullscreen autoplay >' + insideContent + '< /' + tag + '>';
             video_cont.append(video_player);
           } else if (settings.video_src == 'vimeo') {
-            var video_player = '<' + tag + ' src="https://player.vimeo.com/video/' + settings.video_id + '?autoplay=' + videoAutoPlay + '" width="1920" height="1080" layout="responsive" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>' + insideContent + '</' + tag + '>';
+            var video_player = '<' + tag + ' src="https://player.vimeo.com/video/' + settings.video_id + '?autoplay=' + videoAutoPlay + '" width="1920" height="1080" sandbox="allow-scripts allow-same-origin" layout="responsive" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>' + insideContent + '</' + tag + '>';
             video_cont.append(video_player);
           }
         });
